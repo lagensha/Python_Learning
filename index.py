fbdd92318dec5f0d11039a44e2deb4bdbf8fb934
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-15 10:26:09 +0530

    Fix logic for determining the largest and second largest numbers

--- index.py
+++ index.py
@@ -3,4 +3,9 @@ num2=int(input("Enter second number: "))
 num3=int(input("Enter third number: "))
 
 if num1 > num2 :
-    
+    print(num1, "is the largest number")
+    if num2 > num3:
+        print(num2, "is the second largest number")
+else:
+                print(num3, "is the second largest number")
+                
