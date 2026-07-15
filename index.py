28969a8076647890c171f9dc35b7cc21d7da2f4e
Lagensha Rupasinghe <lagensharupasinghe47@gmail.com>
2026-07-15 10:23:37 +0530

    Replace aircraft readiness checks with user input for three numbers

--- index.py
+++ index.py
@@ -1,10 +1,3 @@
-
-    
-eTemp = float(input("Enter the temperature in Fahrenheit: "))
-EoilLevel = float(input("Enter the oil level in liters: "))
-fuelLevel = float(input("Enter the fuel level in liters: "))
-
-if eTemp < 90 and EoilLevel > 60 and fuelLevel > 750000:
-            print("Aircraft is ready for takeoff")
-else:
-    print("Aircraft is not ready for takeoff")
+num1=int(input("Enter first number: "))
+num2=int(input("Enter second number: "))
+num3=int(input("Enter third number: "))
